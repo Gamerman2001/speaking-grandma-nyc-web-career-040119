8534528054db03d16cf1cd182eb05d45499c7bed
Troy Gooden <tro12185@gmail.com>
2019-03-17 17:31:24 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -16,7 +16,7 @@ def speak_to_grandma(say)
   # say = gets.chomp    
   if say == 'I LOVE YOU GRANDMA!'
      'I LOVE YOU TOO PUMPKIN!'
-  elsif say == Class.String.upcase
+  elsif say == say.upcase
      'NO, NOT SINCE 1938!'
   else 
      'HUH?! SPEAK UP, SONNY!'
